34147371a70d29310473eb417f7cfd3dddf80eaf
gusrns48 <gusrns48@naver.com>
2024-06-16 22:07:40 +0900

    게임 시작 전 정보 제공

--- Handlers/ClientRoomManager.cs
+++ Handlers/ClientRoomManager.cs
@@ -2,51 +2,60 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ChatLib.Handlers
 {
-  public class ClientRoomManager
-  {
-    private Dictionary<int, List<ClientHandler>> _roomHandlersDict = new();
-
-    public int Add(ClientHandler clientHandler)
+    public class ClientRoomManager
     {
-      int roomId = clientHandler.InitialData!.RoomId;
+        public Dictionary<int, List<ClientHandler>> _roomHandlersDict = new();
 
-      if (_roomHandlersDict.TryGetValue(roomId, out _)){
-      // 이미 생성된 방에 사용자가 입장할 때
+        //new List
 
-        if(_roomHandlersDict[roomId].Count == 4){
-          // 가득 찼을 때 0 반환
-          return 0;
+        public void Add(ClientHandler clientHandler)
+        {
+            int roomId = clientHandler.InitialData!.RoomId;
+
+            if (_roomHandlersDict.TryGetValue(roomId, out _)) // 이미 있는방 입장
+            {
+                _roomHandlersDict[roomId].Add(clientHandler);
+
+                _roomHandlersDict[roomId].ForEach(x =>
+                    Console.WriteLine(x.InitialData)
+                    // List.Add(x.InitialData.roomId, x.InitialData.userName)
+                ); // 해당 방 사용자 출력
+            }
+            else //최초 생성 입장
+            {
+                _roomHandlersDict[roomId] = new List<ClientHandler>() { clientHandler };
+                // List.Add(x.InitialData.roomId, x.InitialData.userName)
+                _roomHandlersDict[roomId].ForEach(x => Console.WriteLine(x.InitialData)); // 해당 방 사용자 출력
+            }
         }
 
-        _roomHandlersDict[roomId].Add(clientHandler);
-        return _roomHandlersDict[roomId].Count;
-      } else {
-        //첫 방 생성(사용자가 사용하지 않는 방을 지정하고 입장할 때
-        _roomHandlersDict[roomId] = new List<ClientHandler>() { clientHandler };
-        return 1;
-      }
-    }
+        // public void RoomUser(int Id)
+        // {
+        //     return newList
+        // }
 
-    public void Remove(ClientHandler clientHandler)
-    {
-      int roomId = clientHandler.InitialData!.RoomId;
+        public void Remove(ClientHandler clientHandler)
+        {
+            int roomId = clientHandler.InitialData!.RoomId;
 
-      if (_roomHandlersDict.TryGetValue(roomId, out List<ClientHandler>? roomHandlers)) {
-        _roomHandlersDict[roomId] = roomHandlers.FindAll(handler => !handler.Equals(clientHandler));
-      }
-    }
+            if (_roomHandlersDict.TryGetValue(roomId, out List<ClientHandler>? roomHandlers))
+            {
+                _roomHandlersDict[roomId] = roomHandlers.FindAll(handler => !handler.Equals(clientHandler));
+            }
+        }
 
-    public void SendToMyRoom(ChatHub hub)
-    {
-      if (_roomHandlersDict.TryGetValue(hub.RoomId, out List<ClientHandler>? roomHandlers))
-      {
-        roomHandlers.ForEach(handler => handler.Send(hub));
-      }
+        public void SendToMyRoom(ChatHub hub)
+        {
+            if (_roomHandlersDict.TryGetValue(hub.RoomId, out List<ClientHandler>? roomHandlers))
+            {
+                roomHandlers.ForEach(handler => handler.Send(hub));
+            }
+        }
     }
-  }
 }
